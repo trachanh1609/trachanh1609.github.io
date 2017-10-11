deda16c06615ed9175905aac652435ae60b7f1b4
trachanh1609 <Vu@Users-MBP.home>
2017-10-12 00:59:14 +0300

    update

--- portfolio/projects/Weather/res/js/main.js
+++ portfolio/projects/Weather/res/js/main.js
@@ -101,6 +101,7 @@ function getAndDisplayFromServer() {
             method: "GET",
             // url: "http://api.openweathermap.org/data/2.5/find?appid=ffe74f6475c14517ef5e80fe8df0b7d6",
             url: "http://api.openweathermap.org/data/2.5/find?appid=0b96668615bb92609290fa424719a2ea",
+            dataType : "jsonp",
             data: {
                 lat: 60.169847,
                 lon: 24.938340,
